f29b405a40017edfc1935dedb35dbc1120eb41fb
jmisrikhanov <jmisrikhanov@gmail.com>
2022-09-27 22:08:20 +0200

    added button and some styles

--- practice/myDraft/App.js
+++ practice/myDraft/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Button, StyleSheet} from 'react-native';
 
-function App() {
+const App = () => {
+  const alertOutput = label => alert('hey ' + label);
+
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+    <SafeAreaView>
+      <View style={{backgroundColor: 'red'}}>
+        <Text>Hello World</Text>
+        <Text>Hello World 2</Text>
+      </View>
+      <Text style={{color: 'blue'}}>Hello World 3</Text>
+      <Button
+        onPress={() => alertOutput('Jack')}
+        title="press"
+        disabled={false}
+        color="blue"
+      />
+    </SafeAreaView>
   );
-}
+};
 
 export default App;
