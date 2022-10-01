14d3ca0c7ae52f770f1cd1e73886887466bc894b
jmisrikhanov <jmisrikhanov@gmail.com>
2022-10-02 01:13:04 +0200

    setted up the project

---
+++ projects/musicApp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Music App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
