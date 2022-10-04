e373cac5652ba72a9929c7df58e87f51d77471c3
jmisrikhanov <jmisrikhanov@gmail.com>
2022-10-04 13:15:49 +0200

    added react-navigation package

--- practice/myDraft/src/App.js
+++ practice/myDraft/src/App.js
@@ -1,38 +1,13 @@
-// *********************Lifecycle part2******************
+// *********************React Navigation******************
 import React, {useState, useEffect} from 'react';
 import {SafeAreaView, Text, Button, View} from 'react-native';
 
 const App = () => {
-  const [helloFlag, setHelloFlag] = useState(true);
-
-  const updateFlag = () => {
-    setHelloFlag(!helloFlag);
-  };
-
   return (
     <SafeAreaView>
       <Text>Hello Lifecycle</Text>
-      <Button title="Up!" onPress={updateFlag} />
-      {helloFlag && <Hello />}
     </SafeAreaView>
   );
 };
 
 export default App;
-
-const Hello = () => {
-  // Cleanup ===check console=====
-  useEffect(() => {
-    console.log('Hello World');
-
-    return () => {
-      console.log("I'm leaving...");
-    };
-  }, []);
-
-  return (
-    <View style={{backgroundColor: 'aqua', padding: 10}}>
-      <Text>I'm Hello Component</Text>
-    </View>
-  );
-};
