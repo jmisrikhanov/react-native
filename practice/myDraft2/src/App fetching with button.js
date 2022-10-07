ac71811745035a2ad42ed250dec0a6e97beae8d3
jmisrikhanov <jmisrikhanov@gmail.com>
2022-10-08 00:29:28 +0200

    covered data fetching with axios

--- practice/myDraft2/src/App fetching with button.js
+++ practice/myDraft2/src/App fetching with button.js
@@ -1,6 +1,6 @@
 // Web API takes the data from database, processes it and sends it to Frontend
 
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   View,
@@ -15,29 +15,19 @@ import UserCard from './components/UserCard/UserCard';
 const URL = 'http://jsonplaceholder.typicode.com/users';
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [userList, setUserList] = useState([]);
 
   async function fetchData() {
-    // const response = await axios.get(URL);
-    // setLoading(false);
-    // setUserList(response.data);
-
-    // above with then&catch:
-
-    axios.get(URL).then(response => {
-      setLoading(false);
-      setUserList(response.data);
-    });
+    setLoading(true);
+    const response = await axios.get(URL);
+    setLoading(false);
+    setUserList(response.data);
   }
 
   const renderUser = ({item}) => (
     <UserCard name={item.name} email={item.email} username={item.username} />
   );
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <SafeAreaView>
@@ -46,6 +36,7 @@ const App = () => {
       ) : (
         <FlatList data={userList} renderItem={renderUser} />
       )}
+      <Button title="Fetch Data" onPress={fetchData} />
     </SafeAreaView>
   );
 };
