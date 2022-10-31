024f1b151fab310d3545ba85efe59d49e2201a44
jmisrikhanov <jmisrikhanov@gmail.com>
2022-10-31 20:54:36 +0100

    added all dependencies from storeApp

--- projects/storeAppRedux/android/app/src/main/java/com/storeappredux/MainActivity.java
+++ projects/storeAppRedux/android/app/src/main/java/com/storeappredux/MainActivity.java
@@ -4,6 +4,9 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+import android.os.Bundle;
+
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -13,6 +16,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "storeAppRedux";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
